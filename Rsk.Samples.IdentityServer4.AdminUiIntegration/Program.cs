f8ecfe9fd88687a2091e2fc956394463b1e4b895
Scott Brady <scott.brady@rocksolidknowledge.com>
2017-03-28 11:41:31 +0100

    Reintroduced IIS integration switched all environments to use port 5003

--- Rsk.Samples.IdentityServer4.AdminUiIntegration/Program.cs
+++ Rsk.Samples.IdentityServer4.AdminUiIntegration/Program.cs
@@ -8,10 +8,11 @@ namespace Rsk.Samples.IdentityServer4.AdminUiIntegration
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .UseUrls("http://*:5003")
+                .UseIISIntegration()
+                .UseKestrel()
                 .Build();
 
             host.Run();
